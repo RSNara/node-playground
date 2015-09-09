1d875adeec995074a58b5d202a4f2cfdca73e211
Ramanpreet Nara <ramanpreetnara@gmail.com>
2015-09-08 21:58:03 -0400

    Adding chai-as-promised.

--- setup-globals.js
+++ setup-globals.js
@@ -1,7 +1,12 @@
 beforeEach(function(){
 	var chai = require('chai');
-	// setup babel polyfull
-	require('babel/polyfill');
+    var chaiAsPromised = require('chai-as-promised');
+    
+    // setup babel polyfull
+    require('babel/polyfill');
+
+    // setup chai
+    chai.use(chaiAsPromised);
 
 	GLOBAL.expect = chai.expect;
 });
